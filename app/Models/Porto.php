a79bfc9b8906e58544c2b9521b446e0098a88a1c
Rayhan F <rayhanfrdh123@gmail.com>
2024-07-12 15:20:57 +0800

    chore: Update Porto model with file deletion functionality

--- app/Models/Porto.php
+++ app/Models/Porto.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Storage;
 
 class Porto extends Model
 {
@@ -22,4 +23,50 @@ class Porto extends Model
         'tags',
         'url'
     ];
+
+    /**
+     * Deletes a file from the specified file path.
+     *
+     * @param string $filePath The path of the file to be deleted.
+     * @return bool True if the file is successfully deleted, false otherwise.
+     */
+    public function deleteFile($filePath)
+    {
+        if ($filePath) {
+            Storage::disk('public')->delete($filePath);
+        }
+    }
+
+    protected static function boot()
+    {
+        parent::boot();
+
+        /**
+         * Registers a callback to be executed when a Porto model is being deleted.
+         *
+         * @param  \Closure  $callback
+         * @return void
+         */
+        static::deleting(function ($model) {
+            $model->deleteFile($model->image);
+        });
+
+        /**
+         * Register a updating model event with the given callback.
+         *
+         * @param  \Closure  $callback
+         * @return void
+         */
+        static::updating(function ($model) {
+            /**
+             * Checks if the 'image' attribute of the model has been modified.
+             *
+             * @param  Illuminate\Database\Eloquent\Model  $model  The model instance being checked.
+             * @return bool  Returns true if the 'image' attribute has been modified, false otherwise.
+             */
+            if ($model->isDirty('image')) {
+                $model->deleteFile($model->getOriginal('image'));
+            }
+        });
+    }
 }
