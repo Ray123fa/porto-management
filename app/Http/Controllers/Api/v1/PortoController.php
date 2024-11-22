9eddf75e1ce875014d1f294c5dbaaaf9496a2f33
Rayhan F <rayhanfrdh123@gmail.com>
2024-11-23 03:15:43 +0700

    refactor: Rename PortoResource to DataResource and update controller references

--- app/Http/Controllers/Api/v1/PortoController.php
+++ app/Http/Controllers/Api/v1/PortoController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers\Api\v1;
 
 use App\Http\Controllers\Controller;
-use App\Http\Resources\PortoResource;
+use App\Http\Resources\DataResource;
 use App\Models\Porto;
 use Illuminate\Http\Request;
 
@@ -12,16 +12,16 @@ class PortoController extends Controller
     public function index()
     {
         $portos = Porto::paginate(6);
-        return response()->json(new PortoResource(true, 'Seluruh data porto berhasil diambil', $portos));
+        return response()->json(new DataResource(true, 'Seluruh data porto berhasil diambil', $portos));
     }
 
     public function show($id)
     {
         $porto = Porto::find($id);
         if ($porto) {
-            return response()->json(new PortoResource(true, 'Data porto berhasil diambil', $porto));
+            return response()->json(new DataResource(true, 'Data porto berhasil diambil', $porto));
         } else {
-            return response()->json(new PortoResource(false, 'Data porto tidak ditemukan', null), 404);
+            return response()->json(new DataResource(false, 'Data porto tidak ditemukan', null), 404);
         }
     }
 }
