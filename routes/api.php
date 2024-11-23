348f3dbfce6c180df24e61d60e633a8874ae6e7e
Rayhan F <rayhanfrdh123@gmail.com>
2024-11-23 23:59:05 +0700

    feat: Add CORS configuration and update API routes for Experience and Porto controllers

--- routes/api.php
+++ routes/api.php
@@ -1,11 +1,13 @@
 <?php
 
+use App\Http\Controllers\Api\v1\ExperienceController;
+use App\Http\Controllers\Api\v1\PortoController;
 use Illuminate\Support\Facades\Route;
 
 Route::prefix('v1')->middleware('auth:sanctum')->group(function () {
-    Route::apiResource('portos', App\Http\Controllers\Api\v1\PortoController::class)
+    Route::apiResource('portos', PortoController::class)
         ->only(['index', 'show']);
 
-    Route::apiResource('experiences', App\Http\Controllers\Api\v1\ExperienceController::class)
+    Route::apiResource('experiences', ExperienceController::class)
         ->only(['index', 'show']);
 });
